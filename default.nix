daf786b02465a0a22a8245187a37592c522b3775
Rafael Alexander Muijsert <rafael@muijsert.org>
2024-05-03 18:49:49 +0200

    Update default.nix to define user and import home-manager

--- default.nix
+++ default.nix
@@ -1,5 +1,14 @@
 # For use in a NixOS configuration
-{ ... }:
+{ inputs, outputs, config, pkgs, ...}:
 {
+  # Import home manager
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
   
+  users.users.rafael = {
+    isNormalUser = true;
+    extraGroups = [ "wheel", "networkmanager", "video"];
+    shell = pkgs.fish;
+  };
 }
